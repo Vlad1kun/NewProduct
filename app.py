ae9314c92523d871537740babb7fa580ba7523fe
Vlad1kun <109257662+Vlad1kun@users.noreply.github.com>
2023-08-08 19:08:12 +0300

    Flask update

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import telebot
-from asyncio import run, sleep
+from flask import Flask, request
+
 from freeGPT import gpt3
+
+# Создаем экземпляр Flask приложения
+app = Flask(__name__)
 
 # Устанавливаем токен вашего бота
 bot = telebot.TeleBot('6308503974:AAFWhjyFad6zuvljhsVN89eTJlyfKFtmREA')
+
+
+# Обработчик для webhook, принимает POST запросы от Telegram
+@app.route('/', methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return '', 200
+
+
 # Функция для обработки команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.reply_to(message, 'Привет! Я бот, работающий на модели GPT-3.5 Turbo.')
+
 
 # Функция для обработки всех остальных сообщений
 @bot.message_handler(func=lambda message: True)
@@ -18,13 +33,24 @@ def handle_message(message):
     async def process_user_input():
         try:
             # Выполняем запрос к модели GPT-3.5 Turbo
-            resp = await getattr(gpt3, 'Completion')().create(prompt=user_input)
-            bot.reply_to(message, f'🤖: {resp}')
+            resp = await getattr(gpt3.Completion(), 'create')(prompt=user_input)
+            print(f'🤖: {resp.choices[0].text}')  # Выводим сообщение в консоль
+            bot.reply_to(message, f'🤖: {resp.choices[0].text}')
         except Exception as e:
+            print(f'🤖: {e}')  # Выводим сообщение об ошибке в консоль
             bot.reply_to(message, f'🤖: {e}')
 
     # Запускаем асинхронную функцию
-    run(process_user_input())
+    gpt3.setup()
+    gpt3.get_engine('text-davinci-003')
+    gpt3.load_model('gpt-3.5-turbo')
+    app.bot = bot
+    app.user_input = user_input
+    app.loop = asyncio.get_event_loop()
+    app.loop.create_task(process_user_input())
 
-# Запускаем бота
-bot.polling()
+    return '', 200
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
