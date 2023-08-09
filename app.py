ee1736f5ea056a006e16af35a5aae590c2263089
Vlad1kun <109257662+Vlad1kun@users.noreply.github.com>
2023-08-09 07:39:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,27 @@
-import telebot
-from flask import Flask, request
-
+from flask import Flask, render_template, request
+from asyncio import run
 from freeGPT import gpt3
 
-# Создаем экземпляр Flask приложения
 app = Flask(__name__)
 
-# Устанавливаем токен вашего бота
-bot = telebot.TeleBot('6308503974:AAFWhjyFad6zuvljhsVN89eTJlyfKFtmREA')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/process', methods=['POST'])
+def process():
+    user_input = request.form['message']
 
-# Обработчик для webhook, принимает POST запросы от Telegram
-@app.route('/', methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return '', 200
-
-
-# Функция для обработки команды /start
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, 'Привет! Я бот, работающий на модели GPT-3.5 Turbo.')
-
-
-# Функция для обработки всех остальных сообщений
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    user_input = message.text
-
-    # Асинхронная функция для выполнения запроса к модели GPT-3.5 Turbo
     async def process_user_input():
         try:
-            # Выполняем запрос к модели GPT-3.5 Turbo
-            resp = await getattr(gpt3.Completion(), 'create')(prompt=user_input)
-            print(f'🤖: {resp.choices[0].text}')  # Выводим сообщение в консоль
-            bot.reply_to(message, f'🤖: {resp.choices[0].text}')
+            resp = await getattr(gpt3, 'Completion')().create(prompt=user_input)
+            return resp
         except Exception as e:
-            print(f'🤖: {e}')  # Выводим сообщение об ошибке в консоль
-            bot.reply_to(message, f'🤖: {e}')
+            return str(e)
 
-    # Запускаем асинхронную функцию
-    gpt3.setup()
-    gpt3.get_engine('text-davinci-003')
-    gpt3.load_model('gpt-3.5-turbo')
-    app.bot = bot
-    app.user_input = user_input
-    app.loop = asyncio.get_event_loop()
-    app.loop.create_task(process_user_input())
+    response = run(process_user_input())
 
-    return '', 200
-
+    return render_template('index.html', response=response)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
